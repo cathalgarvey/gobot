91bed65b73324f404c1d5cc1e5bfe42f3f814ad1
Cathal Garvey <cathalgarvey@cathalgarvey.me>
2016-02-17 22:10:14 +0000

    Renamed BebopDriver to Driver to propitiate the Lint God. Documented NewBebopDriver func remains unaltered.

--- platforms/bebop/bebop_driver.go
+++ platforms/bebop/bebop_driver.go
@@ -5,7 +5,8 @@ import (
 )
 
 var (
-	_ gobot.Driver = (*BebopDriver)(nil)
+	// TODO: Documentation as to what this achieves would be nice..
+	_ gobot.Driver = (*Driver)(nil)
 	// BebopEvents is a non-exhaustive list of events that may be posted by
 	// an active Bebop object. TODO: Namespace these in-string for sanity?
 	BebopEvents = []string{
@@ -84,8 +85,8 @@ var (
 	}
 )
 
-// BebopDriver is gobot.Driver representation for the Bebop
-type BebopDriver struct {
+// Driver is gobot.Driver representation for the Bebop
+type Driver struct {
 	name       string
 	connection gobot.Connection
 	gobot.Eventer
@@ -93,8 +94,8 @@ type BebopDriver struct {
 }
 
 // NewBebopDriver creates an BebopDriver with specified name.
-func NewBebopDriver(connection *BebopAdaptor, name string) *BebopDriver {
-	d := &BebopDriver{
+func NewBebopDriver(connection *BebopAdaptor, name string) *Driver {
+	d := &Driver{
 		name:         name,
 		connection:   connection,
 		Eventer:      gobot.NewEventer(),
@@ -109,7 +110,7 @@ func NewBebopDriver(connection *BebopAdaptor, name string) *BebopDriver {
 
 // Debug registers a given function to subscribe to all known Bebop Events,
 // including "unknown" and "error"
-func (a *BebopDriver) Debug(f func(string, []byte)) {
+func (a *Driver) Debug(f func(string, []byte)) {
 	for _, e := range BebopEvents {
 		e := e
 		gobot.On(a.Event(e), func(data interface{}) {
@@ -128,29 +129,29 @@ func (a *BebopDriver) Debug(f func(string, []byte)) {
 	}
 }
 
-// Name returns the BebopDrivers Name
-func (a *BebopDriver) Name() string { return a.name }
+// Name returns the Drivers Name
+func (a *Driver) Name() string { return a.name }
 
-// Connection returns the BebopDrivers Connection
-func (a *BebopDriver) Connection() gobot.Connection { return a.connection }
+// Connection returns the Drivers Connection
+func (a *Driver) Connection() gobot.Connection { return a.connection }
 
 // adaptor returns ardrone adaptor
-func (a *BebopDriver) adaptor() *BebopAdaptor {
+func (a *Driver) adaptor() *BebopAdaptor {
 	return a.Connection().(*BebopAdaptor)
 }
 
 // StopTelemetry stops further telemetry messages being posted
 // to the event queue, and closes the channel from the adaptor also.
-func (a *BebopDriver) StopTelemetry() error {
+func (a *Driver) StopTelemetry() error {
 	close(a.endTelemetry)
 	return a.adaptor().drone.StopTelemetry()
 }
 
-// Start starts the BebopDriver.
+// Start starts the Driver.
 // This spins out a goroutine to read telemetry from the adaptor and post
 // events;
-func (a *BebopDriver) Start() (errs []error) {
-	go func(a *BebopDriver) {
+func (a *Driver) Start() (errs []error) {
+	go func(a *Driver) {
 		T := a.adaptor().drone.Telemetry()
 		for {
 			select {
@@ -182,13 +183,14 @@ func (a *BebopDriver) Start() (errs []error) {
 	return
 }
 
-// Halt halts the BebopDriver
-func (a *BebopDriver) Halt() (errs []error) {
+// Halt halts the Driver
+func (a *Driver) Halt() (errs []error) {
+	// TODO: ?
 	return
 }
 
 // TakeOff makes the drone start flying
-func (a *BebopDriver) TakeOff() {
+func (a *Driver) TakeOff() {
 	a.adaptor().drone.TakeOff()
 	// "flying" event should be published by usual event handling system, now
 	// however, it only publishes *after* "takingoff"!
@@ -196,88 +198,88 @@ func (a *BebopDriver) TakeOff() {
 }
 
 // Land causes the drone to land
-func (a *BebopDriver) Land() {
+func (a *Driver) Land() {
 	// TODO: Why is this broken?
 	a.adaptor().drone.Land()
 }
 
 // Up makes the drone gain altitude.
 // speed can be a value from `0` to `100`.
-func (a *BebopDriver) Up(speed int) {
+func (a *Driver) Up(speed int) {
 	a.adaptor().drone.Up(speed)
 }
 
 // Down makes the drone reduce altitude.
 // speed can be a value from `0` to `100`.
-func (a *BebopDriver) Down(speed int) {
+func (a *Driver) Down(speed int) {
 	a.adaptor().drone.Down(speed)
 }
 
 // Left causes the drone to bank to the left, controls the roll, which is
 // a horizontal movement using the camera as a reference point.
 // speed can be a value from `0` to `100`.
-func (a *BebopDriver) Left(speed int) {
+func (a *Driver) Left(speed int) {
 	a.adaptor().drone.Left(speed)
 }
 
 // Right causes the drone to bank to the right, controls the roll, which is
 // a horizontal movement using the camera as a reference point.
 // speed can be a value from `0` to `100`.
-func (a *BebopDriver) Right(speed int) {
+func (a *Driver) Right(speed int) {
 	a.adaptor().drone.Right(speed)
 }
 
 // Forward causes the drone go forward, controls the pitch.
 // speed can be a value from `0` to `100`.
-func (a *BebopDriver) Forward(speed int) {
+func (a *Driver) Forward(speed int) {
 	a.adaptor().drone.Forward(speed)
 }
 
 // Backward causes the drone go forward, controls the pitch.
 // speed can be a value from `0` to `100`.
-func (a *BebopDriver) Backward(speed int) {
+func (a *Driver) Backward(speed int) {
 	a.adaptor().drone.Backward(speed)
 }
 
 // Clockwise causes the drone to spin in clockwise direction
 // speed can be a value from `0` to `100`.
-func (a *BebopDriver) Clockwise(speed int) {
+func (a *Driver) Clockwise(speed int) {
 	a.adaptor().drone.Clockwise(speed)
 }
 
 // CounterClockwise the drone to spin in counter clockwise direction
 // speed can be a value from `0` to `100`.
-func (a *BebopDriver) CounterClockwise(speed int) {
+func (a *Driver) CounterClockwise(speed int) {
 	a.adaptor().drone.CounterClockwise(speed)
 }
 
 // Stop makes the drone to hover in place.
-func (a *BebopDriver) Stop() {
+func (a *Driver) Stop() {
 	a.adaptor().drone.Stop()
 	close(a.endTelemetry)
 }
 
 // Video returns a channel which raw video frames will be broadcast on
-func (a *BebopDriver) Video() chan []byte {
+func (a *Driver) Video() chan []byte {
 	return a.adaptor().drone.Video()
 }
 
 // StartRecording starts the recording video to the drones interal storage
-func (a *BebopDriver) StartRecording() error {
+func (a *Driver) StartRecording() error {
 	return a.adaptor().drone.StartRecording()
 }
 
 // StopRecording stops a previously started recording
-func (a *BebopDriver) StopRecording() error {
+func (a *Driver) StopRecording() error {
 	return a.adaptor().drone.StopRecording()
 }
 
 // HullProtection tells the drone if the hull/prop protectors are attached. This is needed to adjust flight characteristics of the Bebop.
-func (a *BebopDriver) HullProtection(protect bool) error {
+func (a *Driver) HullProtection(protect bool) error {
 	return a.adaptor().drone.HullProtection(protect)
 }
 
 // Outdoor tells the drone if flying Outdoor or not. This is needed to adjust flight characteristics of the Bebop.
-func (a *BebopDriver) Outdoor(outdoor bool) error {
+func (a *Driver) Outdoor(outdoor bool) error {
 	return a.adaptor().drone.Outdoor(outdoor)
 }
